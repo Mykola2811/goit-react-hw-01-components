5dce1110354da3f3f5f1eb42d3596ff7cec4874c
Микола Кондратюк <mykola.kondratiuk@cowchain.io>
2023-05-20 16:21:57 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,11 @@
 
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './Friend/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-
-import user from '../data/user.json';
-import friends from '../data/friends.json';
-import data from '../data/data.json';
-import transactions from '../data/transactions.json';
-
-import { Box } from './BasicStyle.styled';
-
+import {Box} from './BasicStyle.styled';
+import {Header} from './Header/Header';
 
 export const App = () => {
   return (
-
-    <Box>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats} />
-      <Statistics  data={data}/>
-      <FriendList friends={friends}/>
-      <TransactionHistory transactions={transactions}/>
-    </Box>
+    <div>
+      <Header/>
+    </div>
   );
 };
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import { FriendListItem } from './FriendListItem';
+import PropTypes from 'prop-types';
+import Image1 from '../../img/Image1.png';
+import Image2 from '../../img/image2.png';
+import Image3 from '../../img/image3.png';
+import Image4 from '../../img/image4.png';
+import Image5 from '../../img/image5.png';
+import Image6 from '../../img/image6.png';
+import Image7 from '../../img/image7.png';
+import Image8 from '../../img/image8.png';
+import Image9 from '../../img/Frame9.png';
+import Image10 from '../../img/Frame6.png';
+import Image11 from '../../img/Group.png';
+import Image12 from '../../img/Frame1.png';
+export const Header = () => {
+  return <Wrapper >
+      <Ul >
+          <Heder>
+              <img src={Image10} alt="rewards image" width={174} />
+          </Heder>
+          <img src={Image9} alt="rewards image" width={78} height={50} style={{marginBottom: 22}} />
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', paddingRight: 10}}>
+            <img src={Image1} alt="rewards image" width={331} style={{marginBottom: 16, marginRight: 0, display: 'block'}}/>
+            <img src={Image2} alt="rewards image" width={331} style={{marginBottom: 40, marginRight: 0, display: 'block'}}/>
+            <img src={Image3} alt="rewards image" width={331} style={{marginBottom: 40, marginRight: 0, display: 'block'}}/>
+            <img src={Image4} alt="rewards image" width={331} style={{marginBottom: 40, marginRight: 0, display: 'block'}}/>
+            <img src={Image5} alt="rewards image" width={331} style={{ marginBottom: 70, marginRight: 0, display: 'block' }} />
+          </div>
+          <img src={Image12} alt="rewards image" width={87} height={50} style={{marginBottom: 43}}/>
+          <img src={Image6} alt="rewards image" style={{marginBottom: 40}}/>
+          <img src={Image7} alt="rewards image" style={{marginBottom: 55}}/>
+          <img src={Image8} alt="rewards image" width={266} style={{marginBottom: 51}}/>
+          <img src={Image11} alt="rewards image" />
+      </Ul>
+</Wrapper>;
+}
+
+const Wrapper = styled.div`
+    margin-left: 0;
+    margin-top: 0;
+    width: 100%;
+    max-width: 500px;
+`;
+const Heder = styled.div`
+    margin-left: 0;
+    margin-right: 0;
+`;
+const Ul = styled.ul`
+    text-align: center;
+    display: inline-flex;
+    flex-direction: column;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    width: 100%;
+`;
